612ff95e6f0675ba46dbccb71ca03d8a4e8f159c
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2023-07-25 19:04:32 +0530

    a

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const cookieParser = require("cookie-parser")
+
+const authMiddleware = require('./middlewares/authMiddleware');
+const authRoutes = require('./routes/authRoutes');
+const User = require('./models/user');
 
 const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+const url = process.env.MONGO_URL;
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB database!");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB Atlas:", error);
+});
+
+app.use(authMiddleware.authenticateToken);
+
+app.use('/api/auth', authRoutes);
 
 app.get('/test', (req,res) => {
     res.json("test ok");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
