fb4c693fed5567d9c4075fd9dbcb778525ac1ccd
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2023-07-28 18:32:58 +0530

    rewritting frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
-import { useAuth, AuthProvider } from './contexts/AuthContext';
-import UploadFile from './components/UploadFile';
-import CreateFileLink from './components/CreateFileLink';
-import FileLinks from './components/FileLinks';
-import Login from './components/AuthForm';
-import PrivateRoute from './components/PrivateRoute';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/upload">Upload File</Link>
-              </li>
-              <li>
-                <Link to="/create-link">Create File Link</Link>
-              </li>
-              <li>
-                <Link to="/file-links">File Links</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <PrivateRoute path="/upload" component={UploadFile} />
-            <PrivateRoute path="/create-link" component={CreateFileLink} />
-            <PrivateRoute path="/file-links" component={FileLinks} />
-          </Switch>
-        </div>
-      </AuthProvider>
-    </Router>
-  );
-};
-
-const Home = () => {
-  return <h2>Welcome to the File Sharing App!</h2>;
-};
+    <div>App</div>
+  )
+}
 
 export default App;
